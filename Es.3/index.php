67bcc462b07be254e10fbb2120dd21935bf9f993
Arwel219 <aruelangeloriv01@gmail.com>
2025-07-18 12:59:31 +0200

    aggiornamento esercizio php 003

--- Es.3/index.php
+++ Es.3/index.php
@@ -1,20 +1,29 @@
 <?php
 
-function checknum($max)
+function checknumd($max)
 {
     for ($i = 0; $i <= $max; $i++) {
         if ($i % 2 !== 0) {
             echo $i . ', ';
         }
     }
+}
 
-    echo "\n";
-
+function checknump($max)
+{
     for ($i = 0; $i <= $max; $i++) {
         if ($i % 2 === 0) {
             echo $i . ', ';
         }
     }
+}
+
+function checknum($max)
+{
+
+    checknumd($max);
+    echo "\n";
+    checknump($max);
 }
 
 checknum(10);
