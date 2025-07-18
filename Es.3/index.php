5e5614377eb9ea803aa61cea17d55ac44081fda9
Arwel219 <aruelangeloriv01@gmail.com>
2025-07-18 12:40:23 +0200

    aggiornamento esercizi php 003

--- Es.3/index.php
+++ Es.3/index.php
@@ -3,7 +3,7 @@
 function checknum($max)
 {
     for ($i = 0; $i <= $max; $i++) {
-        if ($i % 2 === 0) {
+        if ($i % 2 !== 0) {
             echo $i . ', ';
         }
     }
@@ -11,7 +11,7 @@ function checknum($max)
     echo "\n";
 
     for ($i = 0; $i <= $max; $i++) {
-        if ($i % 2 !== 0) {
+        if ($i % 2 === 0) {
             echo $i . ', ';
         }
     }
